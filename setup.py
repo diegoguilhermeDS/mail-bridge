1f8d41a558d5e9e8cab87ab0eef538fe7e56d3ac
Diego Silva <diego@entrounaconta.com>
2024-08-22 14:26:23 -0300

    Ajsustment to build

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "python-dotenv>=1.0.1",
+        "python-dotenv>=1.0.0",
         "setuptools>=73.0.1"
     ],
     description="A library for sending emails with custom exceptions",
     author="Diego Guilherme",
     author_email="diegoguilherme752@outlook.com",
     url="https://github.com/diegoguilhermeDS/mail-bridge",
-    license="MIT",
+    license="LICENSE",
 )
