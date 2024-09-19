3f1daaa11cf838ac5fdeb48c3503e3a0e7dd35e3
Diego Silva <diego@entrounaconta.com>
2024-09-19 15:20:21 -0300

    Feat: add API MS Graph to send email

--- mail_bridge/main.py
+++ mail_bridge/main.py
@@ -1,109 +1,72 @@
-import dotenv
+import requests
+import logging
+import base64
 import os
-import smtplib
-from datetime import datetime
 
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email.mime.text import MIMEText
-from email import encoders
-
-from .exceptions import (
-    AuthenticationError,
-    EmailEncodingError,
-    SMTPGenericError,
-    SenderRefusedError,
-    RecipientsRefusedError,
-    MailBridgeError,
-    ConfigurationError
-)
-
-# Load environment variables from .env file
-dotenv.load_dotenv()
-
-# Access environment variables
-SENDER = os.getenv("SENDER")
-PASSWORD = os.getenv("PASSWORD_EMAIL_SENDER")
-RECIPIENT = os.getenv("RECIPIENT")
-SMTP_SERVER = "smtp.office365.com"
-PORT = 587
-
-if not all([SENDER, PASSWORD, RECIPIENT]):
-    raise ConfigurationError("SENDER, PASSWORD, and RECIPIENT must all be provided.")
+graph_endpoint = "https://graph.microsoft.com/v1.0"
 
 
-def create_email_message(sender, recipient, subject, body, attachment_path, name_file):
-    """Creates an email message with the specified parameters."""
-    msg = MIMEMultipart()
-    msg["From"] = sender
-    msg["To"] = recipient
-    msg["Subject"] = subject
-
-    # Attach body
-    msg.attach(MIMEText(body, "html"))
-
-    # Attach file
-    if attachment_path:
-        attach_file_to_email(msg, attachment_path, name_file)
-
-    return msg
-
-
-def attach_file_to_email(msg, file_path, name_file):
-    """Attaches a file to the email message."""
-    part = MIMEBase("application", "octet-stream")
-    with open(file_path, 'rb') as file:
-        part.set_payload(file.read())
-    encoders.encode_base64(part)
-
-    today = datetime.today().strftime("%d-%m-%Y")
-    time = datetime.now().strftime('%H-%M-%S')
-    part.add_header(
-        "Content-Disposition",
-        f'attachment; filename="{name_file}-{today}_{time}.xlsx"',
-    )
-    msg.attach(part)
-
-
-def send_email_via_smtp(msg, password):
-    """Sends an email via SMTP with the given message and password."""
+def send_email(access_token, subject, body, recipients, user_email, attachments=None):
     try:
-        server = smtplib.SMTP(SMTP_SERVER, PORT)
-        server.starttls()
-        server.login(msg["From"], password)
-        server.sendmail(msg["From"], msg["To"], msg.as_string().encode("utf-8"))
-    except smtplib.SMTPAuthenticationError:
-        raise AuthenticationError()
+        url = f"{graph_endpoint}/users/{user_email}/sendMail"
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Content-Type": "application/json"
+        }
 
-    except smtplib.SMTPRecipientsRefused:
-        raise RecipientsRefusedError()
+        attachment_data = []
+        if attachments:
+            for attachment in attachments:
+                if not os.path.exists(attachment['file_path']):
+                    raise FileNotFoundError(f"Arquivo {attachment['file_path']} não encontrado")
 
-    except smtplib.SMTPSenderRefused:
-        raise SenderRefusedError()
+                encoded_content = encode_file(attachment['file_path'])
+                attachment_data.append({
+                    "@odata.type": "#microsoft.graph.fileAttachment",
+                    "name": attachment['file_name'],
+                    "contentBytes": encoded_content,
+                    "contentType": attachment['content_type']
+                })
 
-    except smtplib.SMTPException as e:
-        raise SMTPGenericError(f"SMTP error occurred: {str(e)}")
+        email_msg = {
+            "message": {
+                "subject": subject,
+                "body": {
+                    "contentType": "Text",
+                    "content": body
+                },
+                "toRecipients": [{"emailAddress": {"address": recipient}} for recipient in recipients],
+                "attachments": attachment_data
+            },
+            "saveToSentItems": "true"
+        }
 
-    except UnicodeEncodeError as e:
-        raise EmailEncodingError(f"Error encoding email message: {str(e)}")
+        response = requests.post(url, headers=headers, json=email_msg)
 
+        if response.status_code == 202:
+            logging.info("E-mail enviado com sucesso!")
+            return True, "E-mail enviado com sucesso"
+        elif response.status_code == 401:
+            logging.error("Erro de autenticação. Verifique o token.")
+            return False, "Erro de autenticação. Verifique o token."
+        else:
+            logging.error(f"Erro ao enviar e-mail: {response.status_code}, {response.text}")
+            return False, f"Erro ao enviar e-mail: {response.status_code}, {response.text}"
+
+    except requests.exceptions.RequestException as e:
+        logging.exception("Erro de requisição ao enviar e-mail")
+        return False, f"Erro de requisição: {str(e)}"
+    except FileNotFoundError as e:
+        logging.exception("Erro de arquivo não encontrado")
+        return False, f"Erro de arquivo: {str(e)}"
     except Exception as e:
-        raise MailBridgeError(f"An unexpected error occurred: {str(e)}")
-
+        logging.exception("Erro inesperado ao enviar o e-mail")
+        return False, f"Erro inesperado: {str(e)}"
     finally:
-        server.quit()
+        logging.info("Função send_email finalizada.")
 
 
-def send_email(subject, message, attachment_path=None, name_file=None):
-    """Main function to send an email with the Excel file attachment."""
-    body = f"""
-    <html>
-    <body>
-        <p>{message}</p>
-    </body>
-    </html>
-    """
-    msg = create_email_message(SENDER, RECIPIENT, subject, body, attachment_path, name_file)
-    send_email_via_smtp(msg, PASSWORD)
-
-
+def encode_file(file_path):
+    with open(file_path, "rb") as f:
+        encoded = base64.b64encode(f.read()).decode('utf-8')
+    return encoded
